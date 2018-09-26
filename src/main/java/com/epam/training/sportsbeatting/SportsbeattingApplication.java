aa26d85c2ef8e059378acfdb99e9c47af10bd263
Mykola Dekhtiarenko <nnick1212@gmail.com>
2018-09-26 11:36:46 +0300

    initial commit

---
+++ src/main/java/com/epam/training/sportsbeatting/SportsbeattingApplication.java
@@ -0,0 +1,12 @@
+package com.epam.training.sportsbeatting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsbeattingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsbeattingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/epam/training/sportsbeatting/SportsbeattingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.epam.training.sportsbeatting;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SportsbeattingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
