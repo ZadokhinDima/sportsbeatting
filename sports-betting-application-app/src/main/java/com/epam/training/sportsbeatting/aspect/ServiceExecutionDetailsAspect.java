d4a687960d2afeb5f3e664b1812329005bea318d
Dmytro Zadokhin <Dmytro_Zadokhin@epam.com>
2019-01-08 18:23:00 +0200

    - add AOP to project - create aspect to log info about service methods execution

---
+++ sports-betting-application-app/src/main/java/com/epam/training/sportsbeatting/aspect/ServiceExecutionDetailsAspect.java
@@ -0,0 +1,48 @@
+package com.epam.training.sportsbeatting.aspect;
+
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ServiceExecutionDetailsAspect {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
+    @Around("serviceLayerMethods()")
+    private Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        long startTime = System.currentTimeMillis();
+        final Object methodResult = joinPoint.proceed();
+        long timeTaken = System.currentTimeMillis() - startTime;
+
+        logger.info("Method {} took {} ms.", joinPoint.getSignature().toString(), timeTaken);
+        return methodResult;
+    }
+
+    @Before("serviceLayerMethods()")
+    private void logMethodNameAndArgs(JoinPoint joinPoint) {
+        logger.info("Method {} called with parameters {}.", joinPoint.getSignature().toString(),
+                Arrays.toString(joinPoint.getArgs()));
+    }
+
+    @AfterReturning(pointcut = "serviceLayerMethods()", returning = "result")
+    private void logMethodResult(JoinPoint joinPoint, Object result) {
+        logger.info("Method {} returned result {}.", joinPoint.getSignature().toString(), result.toString());
+    }
+
+    @Pointcut("@within(org.springframework.stereotype.Service)")
+    private void serviceLayerMethods() {}
+
+}
